655b2081f14a0add2e998e2bf47d9743af19b341
uvsq21501650 <maturinbado@gmail.com>
2015-11-10 02:20:18 +0100

    Second test : Suppression des varables n et pins dans testGutterGame

--- ex3_2/BowlingGame/src/test/java/BowlingGameTest.java
+++ ex3_2/BowlingGame/src/test/java/BowlingGameTest.java
@@ -61,9 +61,7 @@ public class BowlingGameTest extends TestCase {
       @Test
       public void testGutterGame() throws Exception
       {
-          int n=20;
-          int pins=0;
-          rollMany(n, pins);
+          rollMany(20, 0);
          assertEquals(0, g.score());
       }
       private void rollMany(int n, int pins)
