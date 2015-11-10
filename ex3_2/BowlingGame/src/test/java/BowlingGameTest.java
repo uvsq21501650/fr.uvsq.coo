beda8ed44c12bcd2444735627fbe79537a6f32b8
uvsq21501650 <maturinbado@gmail.com>
2015-11-10 02:11:22 +0100

    Second Test : Amelioration de TestGutterGame

--- ex3_2/BowlingGame/src/test/java/BowlingGameTest.java
+++ ex3_2/BowlingGame/src/test/java/BowlingGameTest.java
@@ -61,8 +61,10 @@ public class BowlingGameTest extends TestCase {
       @Test
       public void testGutterGame() throws Exception
       {
-         for(int i=0; i<20;i++)
-             g.roll(0);
+          int n=20;
+          int pins=0;
+         for(int i=0; i<n;i++)
+             g.roll(pins);
          assertEquals(0, g.score());
       }
       
