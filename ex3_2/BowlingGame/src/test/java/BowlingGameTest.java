c11bfb5160de2d4875f5cc1259db2f3faf1ccb56
uvsq21501650 <maturinbado@gmail.com>
2015-11-10 02:23:07 +0100

    Second Test : Suppression de la boucle dans testAllOnes

--- ex3_2/BowlingGame/src/test/java/BowlingGameTest.java
+++ ex3_2/BowlingGame/src/test/java/BowlingGameTest.java
@@ -73,8 +73,7 @@ public class BowlingGameTest extends TestCase {
       @Test
       public void testAllOnes () throws Exception
       {
-          for(int i=0; i<20; i++)
-              g.roll(1);
+          rollMany(20, 1);
           assertEquals(20, g.score());
       }
 }
