5b57661e72a7de762f3a0d32b3d39685b361aa26
uvsq21501650 <maturinbado@gmail.com>
2015-11-12 00:53:44 +0100

    Quatrieme test : ajout de isStrike() ...

--- ex3_2/BowlingGame/src/main/java/com/uvsq/groupe3/bowlinggame/Game.java
+++ ex3_2/BowlingGame/src/main/java/com/uvsq/groupe3/bowlinggame/Game.java
@@ -30,27 +30,28 @@ public class Game {
         int frameIndex=0;
         for(int frame=0; frame < 10;  frame++)
         {
-            if(rolls[frameIndex] == 10) //spare
+            if(isStrike(frameIndex)) //spare
             {
-                score += 10 + 
-                        rolls[frameIndex+1] +
-                        rolls[frameIndex + 2];
+                score += 10 + strikeBonus(frameIndex);
                 frameIndex ++;
             }
             else if(isSpare(frameIndex))
             {
-                score += 10 + rolls[frameIndex + 2];
-                frameIndex += 2;
+                score += 10 + spareBonus(frameIndex);
             }
             else
             {
-                score += rolls[frameIndex] + 
-                             rolls[frameIndex+1];
+                score += sumOfBallsInFrame(frameIndex);
                 frameIndex += 2;
             }
         }
         return score;
     }
+    private boolean isStrike(int frameIndex) 
+    {
+        return rolls[frameIndex] == 10;
+    }
+    
     private int sumOfBallsInFrame(int frameIndex) 
     {
         return rolls[frameIndex]+rolls[frameIndex+1];
