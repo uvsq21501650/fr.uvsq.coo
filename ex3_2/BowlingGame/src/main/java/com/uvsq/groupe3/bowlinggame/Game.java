554ccb9e09df06ec0899b965e3f80c0c59fd5fec
uvsq21501650 <maturinbado@gmail.com>
2015-11-12 01:00:34 +0100

    Quatrieme test : corrections d'erreurs

--- ex3_2/BowlingGame/src/main/java/com/uvsq/groupe3/bowlinggame/Game.java
+++ ex3_2/BowlingGame/src/main/java/com/uvsq/groupe3/bowlinggame/Game.java
@@ -28,9 +28,9 @@ public class Game {
     {
         int score=0;
         int frameIndex=0;
-        for(int frame=0; frame < 10;  frame++)
+        for(int frame = 0; frame < 10;  frame++)
         {
-            if(isStrike(frameIndex)) //spare
+            if(isStrike(frameIndex)) 
             {
                 score += 10 + strikeBonus(frameIndex);
                 frameIndex ++;
@@ -38,6 +38,7 @@ public class Game {
             else if(isSpare(frameIndex))
             {
                 score += 10 + spareBonus(frameIndex);
+                frameIndex += 2;
             }
             else
             {
